d53d51e8b2a6399819e108109d696f3b1700ae7a
timel2ss <dyddn0823@hanyang.ac.kr>
2024-01-15 00:42:03 +0900

    chore: rest docs 설정

---
+++ module-api/src/test/java/me/shovelog/RestDocs.java
@@ -0,0 +1,38 @@
+package me.shovelog;
+
+import me.shovelog.category.controller.CategoryController;
+import me.shovelog.category.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@ActiveProfiles("test")
+@WebMvcTest(controllers = {CategoryController.class})
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocs {
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(WebApplicationContext context,
+               RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(provider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+
+    @MockBean
+    protected CategoryService categoryService;
+}
