6d0928fe19baacc16777e7859738b9a34237eb89
timel2ss <dyddn0823@hanyang.ac.kr>
2024-02-06 00:39:35 +0900

    test: 카테고리 목록 조회 기능 테스트
    카테고리 아이템 조회 로직 추가

--- module-api/src/test/java/me/shovelog/category/controller/CategoryControllerTest.java
+++ module-api/src/test/java/me/shovelog/category/controller/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package me.shovelog.category.controller;
 
 import me.shovelog.RestDocs;
+import me.shovelog.category.service.response.CategoryItemResponse;
 import me.shovelog.category.service.response.CategoryResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,17 +23,27 @@ class CategoryControllerTest extends RestDocs {
     @DisplayName("카테고리 목록 조회 API")
     void getCategories() throws Exception {
         // given
+        CategoryItemResponse categoryItem1 = createCategoryItem(1L, "React", "리액트 설명");
+        CategoryItemResponse categoryItem2 = createCategoryItem(2L, "Vue", "뷰 설명");
+        CategoryItemResponse categoryItem3 = createCategoryItem(3L, "Spring", "스프링 설명");
+        CategoryItemResponse categoryItem4 = createCategoryItem(4L, "JVM", "JVM 설명");
+        CategoryItemResponse categoryItem5 = createCategoryItem(5L, "NestJS", "NestJS 설명");
+        CategoryItemResponse categoryItem6 = createCategoryItem(6L, "DBMS", "DBMS 설명");
+        CategoryItemResponse categoryItem7 = createCategoryItem(7L, "SQL", "SQL 설명");
+
         List<CategoryResponse> response = List.of(
                 createCategory(1L,
                         "프론트엔드",
-                        "웹 프론트엔드 개발 일기"),
+                        "웹 프론트엔드 개발 일기",
+                        List.of(categoryItem1, categoryItem2)),
                 createCategory(2L,
                         "백엔드",
-                        "백엔드 개발 일기"),
+                        "백엔드 개발 일기",
+                        List.of(categoryItem3, categoryItem4, categoryItem5)),
                 createCategory(3L,
                         "데이터베이스",
-                        "데이터베이스 학습 일기")
-        );
+                        "데이터베이스 학습 일기",
+                        List.of(categoryItem6, categoryItem7)));
         given(categoryService.findAll()).willReturn(response);
 
         // when then
@@ -41,22 +52,44 @@ class CategoryControllerTest extends RestDocs {
                 .andDo(print())
                 .andDo(document("category-list",
                         responseFields(
+                                fieldWithPath("[]").type(JsonFieldType.ARRAY)
+                                        .description("카테고리 목록"),
                                 fieldWithPath("[].id").type(JsonFieldType.NUMBER)
                                         .description("카테고리 ID"),
                                 fieldWithPath("[].name").type(JsonFieldType.STRING)
                                         .description("카테고리 이름"),
                                 fieldWithPath("[].description").type(JsonFieldType.STRING)
-                                        .description("카테고리 설명")
+                                        .description("카테고리 설명"),
+                                fieldWithPath("[].items[]").type(JsonFieldType.ARRAY)
+                                        .description("카테고리 아이템 목록"),
+                                fieldWithPath("[].items[].id").type(JsonFieldType.NUMBER)
+                                        .description("카테고리 아이템 ID"),
+                                fieldWithPath("[].items[].itemName").type(JsonFieldType.STRING)
+                                        .description("카테고리 아이템 이름"),
+                                fieldWithPath("[].items[].description").type(JsonFieldType.STRING)
+                                        .description("카테고리 아이템 설명")
                         )
                 ));
     }
 
     private CategoryResponse createCategory(long id,
                                             String name,
-                                            String description) {
+                                            String description,
+                                            List<CategoryItemResponse> items) {
         return CategoryResponse.builder()
                 .id(id)
                 .name(name)
+                .description(description)
+                .items(items)
+                .build();
+    }
+
+    private CategoryItemResponse createCategoryItem(long id,
+                                                    String itemName,
+                                                    String description) {
+        return CategoryItemResponse.builder()
+                .id(id)
+                .itemName(itemName)
                 .description(description)
                 .build();
     }
--- module-api/src/test/java/me/shovelog/category/service/CategoryServiceTest.java
+++ module-api/src/test/java/me/shovelog/category/service/CategoryServiceTest.java
@@ -2,12 +2,16 @@ package me.shovelog.category.service;
 
 import me.shovelog.ServiceTest;
 import me.shovelog.category.domain.Category;
+import me.shovelog.category.domain.CategoryItem;
+import me.shovelog.category.repository.CategoryItemRepository;
 import me.shovelog.category.repository.CategoryRepository;
+import me.shovelog.category.service.response.CategoryItemResponse;
 import me.shovelog.category.service.response.CategoryResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,24 +25,52 @@ class CategoryServiceTest extends ServiceTest {
     @Autowired
     CategoryRepository categoryRepository;
 
+    @Autowired
+    CategoryItemRepository categoryItemRepository;
+
     @Test
     @DisplayName("카테고리 목록 조회")
     void findAllCategories() {
         // given
         Category category1 = createCategory("프론트엔드", "웹 프론트엔드 개발 일기");
+        CategoryItem react = createCategoryItem(category1, "React", "리액트 설명");
+        CategoryItem vue = createCategoryItem(category1, "Vue", "뷰 설명");
+
         Category category2 = createCategory("백엔드", "백엔드 개발 일기");
+        CategoryItem spring = createCategoryItem(category2, "Spring", "스프링 설명");
+        CategoryItem jvm = createCategoryItem(category2, "JVM", "JVM 설명");
+        CategoryItem nestJs = createCategoryItem(category2, "NestJS", "NestJS 설명");
+
         Category category3 = createCategory("데이터베이스", "데이터베이스 학습 일기");
+        CategoryItem dbms = createCategoryItem(category3, "DBMS", "DBMS 설명");
+        CategoryItem sql = createCategoryItem(category3, "SQL", "SQL 설명");
 
         // when
-        List<CategoryResponse> response = categoryService.findAll();
+        List<CategoryResponse> categoryResponse = categoryService.findAll();
 
         // then
-        assertThat(response).hasSize(3);
-        assertThat(response).extracting("id", "name", "description")
+        List<CategoryItemResponse> categoryItemResponse = categoryResponse.stream()
+                .map(CategoryResponse::items)
+                .flatMap(Collection::stream)
+                .toList();
+
+        assertThat(categoryResponse).hasSize(3);
+        assertThat(categoryResponse).extracting("id", "name", "description")
                 .containsExactlyInAnyOrder(
                         tuple(category1.getId(), category1.getName(), category1.getDescription()),
                         tuple(category2.getId(), category2.getName(), category2.getDescription()),
                         tuple(category3.getId(), category3.getName(), category3.getDescription())
+                );
+        assertThat(categoryItemResponse).hasSize(7);
+        assertThat(categoryItemResponse).extracting("id", "itemName", "description")
+                .containsExactlyInAnyOrder(
+                        tuple(react.getId(), react.getItemName(), react.getDescription()),
+                        tuple(vue.getId(), vue.getItemName(), vue.getDescription()),
+                        tuple(spring.getId(), spring.getItemName(), spring.getDescription()),
+                        tuple(jvm.getId(), jvm.getItemName(), jvm.getDescription()),
+                        tuple(nestJs.getId(), nestJs.getItemName(), nestJs.getDescription()),
+                        tuple(dbms.getId(), dbms.getItemName(), dbms.getDescription()),
+                        tuple(sql.getId(), sql.getItemName(), sql.getDescription())
                 );
     }
 
@@ -48,5 +80,15 @@ class CategoryServiceTest extends ServiceTest {
                         .name(name)
                         .description(description)
                         .build());
+    }
+
+    private CategoryItem createCategoryItem(Category category, String itemName, String description) {
+        CategoryItem item = categoryItemRepository.save(CategoryItem.builder()
+                .category(category)
+                .itemName(itemName)
+                .description(description)
+                .build());
+        category.addItem(item);
+        return item;
     }
 }
