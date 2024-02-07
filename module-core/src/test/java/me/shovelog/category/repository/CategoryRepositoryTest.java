345e8cd1e7b0c4ef88c0119a7e93c5e612157a69
timel2ss <dyddn0823@hanyang.ac.kr>
2024-02-07 16:55:00 +0900

    test: 카테고리 순서 정렬

--- module-core/src/test/java/me/shovelog/category/repository/CategoryRepositoryTest.java
+++ module-core/src/test/java/me/shovelog/category/repository/CategoryRepositoryTest.java
@@ -24,18 +24,18 @@ class CategoryRepositoryTest extends RepositoryTest {
     @DisplayName("카테고리 조회 시 카테고리의 하위 아이템을 패치 조인으로 한꺼번에 가져와야 한다")
     void findAllFetchJoin() {
         // given
-        Category frontend = createCategory("프론트엔드", "프론트엔드 설명");
-        CategoryItem react = createCategoryItem(frontend, "React", "리액트 설명");
-        CategoryItem vue = createCategoryItem(frontend, "Vue", "뷰 설명");
+        Category frontend = createCategory("프론트엔드", "프론트엔드 설명", 1L);
+        CategoryItem react = createCategoryItem(frontend, "React", "리액트 설명", 1L);
+        CategoryItem vue = createCategoryItem(frontend, "Vue", "뷰 설명", 2L);
 
-        Category backend = createCategory("백엔드", "백엔드 설명");
-        CategoryItem spring = createCategoryItem(backend, "Spring", "스프링 설명");
-        CategoryItem jvm = createCategoryItem(backend, "JVM", "JVM 설명");
-        CategoryItem nestJs = createCategoryItem(backend, "NestJS", "NestJS 설명");
+        Category backend = createCategory("백엔드", "백엔드 설명", 3L);
+        CategoryItem spring = createCategoryItem(backend, "Spring", "스프링 설명", 2L);
+        CategoryItem jvm = createCategoryItem(backend, "JVM", "JVM 설명", 1L);
+        CategoryItem nestJs = createCategoryItem(backend, "NestJS", "NestJS 설명", 3L);
 
-        Category algorithm = createCategory("알고리즘", "알고리즘 설명");
-        CategoryItem boj = createCategoryItem(algorithm, "BOJ", "백준 설명");
-        CategoryItem leetCode = createCategoryItem(backend, "LeetCode", "LeetCode 설명");
+        Category algorithm = createCategory("알고리즘", "알고리즘 설명", 2L);
+        CategoryItem boj = createCategoryItem(algorithm, "BOJ", "백준 설명", 2L);
+        CategoryItem leetCode = createCategoryItem(algorithm, "LeetCode", "LeetCode 설명", 1L);
 
         // when
         List<Category> categories = categoryRepository.findAllFetchJoin();
@@ -48,34 +48,36 @@ class CategoryRepositoryTest extends RepositoryTest {
 
         assertThat(categories).hasSize(3);
         assertThat(categories).extracting("name", "description")
-                .contains(
+                .containsExactly(
                         tuple(frontend.getName(), frontend.getDescription()),
-                        tuple(backend.getName(), backend.getDescription()),
-                        tuple(algorithm.getName(), algorithm.getDescription()));
+                        tuple(algorithm.getName(), algorithm.getDescription()),
+                        tuple(backend.getName(), backend.getDescription()));
         assertThat(categoryItems).hasSize(7);
         assertThat(categoryItems).extracting("itemName", "description")
-                .contains(
+                .containsExactly(
                         tuple(react.getItemName(), react.getDescription()),
                         tuple(vue.getItemName(), vue.getDescription()),
-                        tuple(spring.getItemName(), spring.getDescription()),
-                        tuple(jvm.getItemName(), jvm.getDescription()),
-                        tuple(nestJs.getItemName(), nestJs.getDescription()),
+                        tuple(leetCode.getItemName(), leetCode.getDescription()),
                         tuple(boj.getItemName(), boj.getDescription()),
-                        tuple(leetCode.getItemName(), leetCode.getDescription()));
+                        tuple(jvm.getItemName(), jvm.getDescription()),
+                        tuple(spring.getItemName(), spring.getDescription()),
+                        tuple(nestJs.getItemName(), nestJs.getDescription()));
     }
 
-    private Category createCategory(String name, String description) {
+    private Category createCategory(String name, String description, Long categoryOrder) {
         return save(Category.builder()
                 .name(name)
                 .description(description)
+                .categoryOrder(categoryOrder)
                 .build());
     }
 
-    private CategoryItem createCategoryItem(Category category, String itemName, String description) {
+    private CategoryItem createCategoryItem(Category category, String itemName, String description, Long itemOrder) {
         CategoryItem item = save(CategoryItem.builder()
                 .category(category)
                 .itemName(itemName)
                 .description(description)
+                .itemOrder(itemOrder)
                 .build());
         category.addItem(item);
         return item;
