d90f59d4a0ebfc32f7090c6bb0b0dda161473caa
timel2ss <dyddn0823@hanyang.ac.kr>
2024-02-09 21:40:23 +0900

    feat: 테스트 util 개선

--- module-core/src/test/java/me/shovelog/RepositoryTest.java
+++ module-core/src/test/java/me/shovelog/RepositoryTest.java
@@ -1,47 +1,28 @@
 package me.shovelog;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
 public abstract class RepositoryTest {
-
-    @Autowired
-    protected EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    protected EntityManager em;
 
     protected <T> T save(T entity) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        try {
-            em.persist(entity);
-            em.flush();
-            tx.commit();
-            em.clear();
-        } catch (Exception e) {
-            tx.rollback();
-        }
+        em.persist(entity);
+        em.flush();
+        em.clear();
         return entity;
     }
 
     protected <T> Iterable<T> saveAll(Iterable<T> entities) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
         for (T entity : entities) {
-            try {
-                em.persist(entity);
-                em.flush();
-                tx.commit();
-                em.clear();
-            } catch (Exception e) {
-                tx.rollback();
-            }
+            em.persist(entity);
+            em.flush();
+            em.clear();
         }
         return entities;
     }
